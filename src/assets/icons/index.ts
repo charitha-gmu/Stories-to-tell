7e6bed3726b09d194061be4dc42c9a122e271ca5
charitha-meka <mekacharitha.19@gmail.com>
2023-09-07 00:10:49 -0400

    Footer changes

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+import Instagram from './instagram-svgrepo-com.svg';
+import Facebook from "./facebook-svgrepo-com.svg";
+import Twitter from "./twitter-svgrepo-com.svg";
+
+export { Instagram, Facebook,  Twitter}
