15aa960efdd092be694291b17eb200631ddc79cb
wal-saikiran <saikiran.k@westagilelabs.com>
2023-09-24 17:14:11 -0400

    Firebase deployment fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import "./App.css";
 import "./_global.scss";
 import AppRoutes from "routes";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import HelpInterface from "components/HelpInterface";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter>
         <AppRoutes />
         <HelpInterface />
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
